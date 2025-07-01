995f4a1bd63876ddd06b7c66de2935f5e3cbf4fa
Roman Telezhynskyi <dismine@gmail.com>
2025-07-01 17:58:57 +0300

    fix: find the most specific libstdc++ version If we match the first found on Debian, then we're likely to find only version 6, without minor, which is present on basically any system. However we need a precise match

--- appimagebuilder/modules/setup/apprun_3/helpers/glibstcpp_module.py
+++ appimagebuilder/modules/setup/apprun_3/helpers/glibstcpp_module.py
@@ -26,22 +26,35 @@ class AppRun3GLibStdCppSetupHelper(AppRun3Helper):
         self._glibstdcpp_module_files = []
 
     def run(self):
-        self._glibstdcpp_module_files = self.context.app_dir.find(file_matching_patterns.glibstdcpp)
+        self._glibstdcpp_module_files = self.context.app_dir.find(
+            file_matching_patterns.glibstdcpp
+        )
 
         if self._glibstdcpp_module_files:
             self._module_dir.mkdir(parents=True, exist_ok=True)
 
-            self.context.app_dir.move_files(self._glibstdcpp_module_files, self._module_dir)
+            self.context.app_dir.move_files(
+                self._glibstdcpp_module_files, self._module_dir
+            )
             self._deploy_check_glibstdcpp_binary()
 
             libstdcpp_version = self._extract_libstdcpp_version()
-            library_paths = set([entry.path.parent for entry in self._glibstdcpp_module_files if entry.soname])
+            library_paths = set(
+                [
+                    entry.path.parent
+                    for entry in self._glibstdcpp_module_files
+                    if entry.soname
+                ]
+            )
 
             self._generate_glibstdcpp_module_config(libstdcpp_version, library_paths)
 
     def _deploy_check_glibstdcpp_binary(self):
-        glibstdcpp_check_binary_path = self.context.binaries_resolver.resolve_check_glibstdcpp_binary(
-            self.context.main_arch)
+        glibstdcpp_check_binary_path = (
+            self.context.binaries_resolver.resolve_check_glibstdcpp_binary(
+                self.context.main_arch
+            )
+        )
         glibstdcpp_check_binary_target_path = self._module_dir / "check"
 
         # ensure the target directory exists
@@ -54,7 +67,10 @@ class AppRun3GLibStdCppSetupHelper(AppRun3Helper):
         os.chmod(glibstdcpp_check_binary_target_path, 0o755)
 
     def _generate_glibstdcpp_module_config(self, libstdcpp_version, library_paths):
-        library_paths = [replace_app_dir_in_path(self.context.app_dir.path, path) for path in library_paths]
+        library_paths = [
+            replace_app_dir_in_path(self.context.app_dir.path, path)
+            for path in library_paths
+        ]
 
         config = {
             "version": "1.0",
@@ -73,8 +89,8 @@ class AppRun3GLibStdCppSetupHelper(AppRun3Helper):
     def _extract_libstdcpp_version(self):
         version = None
         for entry in self._glibstdcpp_module_files:
-            if entry.soname == 'libstdc++.so.6':
+            if entry.soname == "libstdc++.so.6" and not entry.path.is_symlink():
                 # extract libstdc++ version from file name
-                version = entry.path.name.split('.so.')[-1]
+                version = entry.path.name.split(".so.")[-1]
                 break
         return version
